d9f337487d78c45eb32f982a5a8684b5c5370c2d
Abhay Verma <abhayv175@gmail.com>
2026-03-29 02:58:57 +0000

    idk

--- components/about.tsx
+++ components/about.tsx
@@ -18,23 +18,18 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
+
+
+
+
+
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        I am
       </p>
+
+
+
+
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
@@ -44,6 +39,10 @@ export default function About() {
         <span className="font-medium">history and philosophy</span>. I'm also
         learning how to play the guitar.
       </p>
+
+
+
+
     </motion.section>
   );
 }
